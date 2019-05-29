453c27ffe33b61f0d5db1311aa7f9b8a4082e542
“NaimKhanSiam” <naim_khan@live.co.uk>
2019-05-29 19:48:57 -0400

    updates

---
+++ UiCatalog/src/main/java/testCases/RegressionTestCases.java
@@ -0,0 +1,10 @@
+package testCases;
+
+import base.MobileAPI2;
+
+public class RegressionTestCases extends MobileAPI2 {
+
+    public void method1() {
+        System.out.println(appiumDriver.getPageSource());
+    }
+}
---
+++ UiCatalog/src/test/java/testcasesIOS.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+import testCases.RegressionTestCases;
+
+public class testcasesIOS extends RegressionTestCases {
+
+    @Test
+    public void testt(){
+        method1();
+    }
+}
